df8c37f4af89a2aabc4da89541002197ae7e613c
richardmiller <rmiller@inviqa.com>
2017-01-11 17:31:32 +0000

    Update front controller to use env variables

--- web/app.php
+++ web/app.php
@@ -1,14 +1,37 @@
 <?php
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Debug\Debug;
+
+$environment = getenv('SYMFONY_ENV');
+if ($environment === false) {
+    $environment = 'prod';
+}
+
+if (($useDebugging = getenv('SYMFONY_DEBUG')) === false || $useDebugging === '') {
+    $useDebugging = $environment === 'dev';
+}
 
 /** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__.'/../app/autoload.php';
-include_once __DIR__.'/../var/bootstrap.php.cache';
 
-$kernel = new AppKernel('prod', false);
-$kernel->loadClassCache();
-//$kernel = new AppCache($kernel);
+if (!$useDebugging) {
+    include_once __DIR__ . '/../var/bootstrap.php.cache';
+}
+
+require_once __DIR__ . '/../app/AppKernel.php';
+
+if ($useDebugging) {
+    Debug::enable();
+}
+
+$kernel = new AppKernel($environment, $useDebugging);
+
+// we don't want to use the classes cache if we are in a debug session
+if (!$useDebugging) {
+    $kernel->loadClassCache();
+}
+
 
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
 //Request::enableHttpMethodParameterOverride();
